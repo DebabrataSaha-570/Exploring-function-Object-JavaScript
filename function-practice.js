f924171f26f3f3536658597142e6c65e578df6a6
Debabrata Saha <sahadebabrata570@gmail.com>
2021-08-20 14:40:31 +0600

    learned how to write a function

---
+++ function-practice.js
@@ -0,0 +1,26 @@
+function kobutor() {
+    console.log('give them water')
+    console.log('clean their room')
+    console.log('take them shower')
+    console.log('observe them ')
+}
+
+kobutor()
+
+
+function makeRoti() {
+    console.log('boil water for 10 min')
+    console.log('take moyda')
+    console.log('mix them with water')
+    console.log('ekhn roti bele felo')
+    console.log('roti cheke felo')
+}
+makeRoti()
+
+
+function toiletClean() {
+    console.log('mix harpic for 20 min ')
+    console.log('clean with water and brush')
+
+}
+toiletClean()
