0bc9e3a4c432f0105154200110ae3a82ce1db2fb
Debabrata Saha <sahadebabrata570@gmail.com>
2021-08-21 04:09:45 +0600

    learned break and continue in while and for loop

---
+++ break-continue.js
@@ -0,0 +1,43 @@
+var i = 1;
+while (i < 10) {
+    console.log(i);
+    if (i == 5) {
+        break;
+    }
+    i++;
+}
+
+
+
+for (var i = 0; i < 20; i++) {
+    console.log(i);
+    if (i == 8) {
+        break
+    }
+}
+
+
+
+
+var numbers = [54, 35, 21, 78, 98, 23, 101]
+for (var i = 0; i < numbers.length; i++) {
+    var number = numbers[i];
+    console.log(number)
+    if (number > 90) {
+        break;
+    }
+}
+
+
+
+
+
+
+var numbers = [54, 35, 21, 78, 98, 23, 101]
+for (var i = 0; i < numbers.length; i++) {
+    var number = numbers[i]
+    if (number > 90) {
+        continue
+    }
+    console.log(number)
+}
