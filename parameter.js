4e8355c5223e096df047951049dbeef964170b91
Debabrata Saha <sahadebabrata570@gmail.com>
2021-08-20 15:35:21 +0600

    learned function parameter

---
+++ parameter.js
@@ -0,0 +1,10 @@
+function bringSingara(money) {
+    console.log('singarar jonno dise', money)
+    console.log('mama singara den')
+    var singaraPrice = 10;
+    var singraQuantity = taka / singaraPrice;
+    return singraQuantity
+}
+var taka = 260;
+var singara = bringSingara(taka)
+console.log('ei nen singara', singara)
---
+++ parameterPractice.js
@@ -0,0 +1,10 @@
+function bringMedicine(money) {
+    console.log('dada ei osud ta ache?')
+    console.log('eitar dam koto? ')
+    var quantity = 10;
+    var medicine = money / quantity
+    return medicine;
+}
+
+var osud = bringMedicine(300)
+console.log(osud)
